8e55e6c3a6c7d5ec4855e293224712db273dd30a
jalv <jalv82@gmail.com>
2022-05-13 23:40:17 +0200

    Kata Fizz Buzz is added

---
+++ fizzbuzz/fizzbuzz_test.go
@@ -0,0 +1,47 @@
+package fizzbuzz
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testCases = []struct {
+	input    int
+	expected string
+}{
+	{1, "1"},
+	{2, "2"},
+	{3, "Fizz"},
+	{4, "4"},
+	{5, "Buzz"},
+	{6, "Fizz"},
+	{7, "7"},
+	{8, "8"},
+	{9, "Fizz"},
+	{10, "Buzz"},
+	{11, "11"},
+	{12, "Fizz"},
+	{13, "13"},
+	{14, "14"},
+	{15, "FizzBuzz"},
+	{16, "16"},
+	{17, "17"},
+	{18, "Fizz"},
+	{19, "19"},
+	{20, "Buzz"},
+}
+
+func TestFizzBuzz(t *testing.T) {
+	for _, tt := range testCases {
+		tt := tt // NOTE: https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+		testName := fmt.Sprintf("should return %s when input is %d", tt.expected, tt.input)
+
+		t.Run(testName, func(t *testing.T) {
+			// When
+			result := FizzBuzz(tt.input)
+			// Then
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
