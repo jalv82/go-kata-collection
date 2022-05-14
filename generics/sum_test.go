18bba0517ed26acd65b04febb9dc250d74f09d81
jalv <jalv82@gmail.com>
2022-05-14 22:09:45 +0200

    Sum any quantity of numbers of type float

--- generics/sum_test.go
+++ generics/sum_test.go
@@ -11,7 +11,7 @@ type testsTable[T MyNumber] struct {
 	want        T
 }
 
-func TestSum(t *testing.T) {
+func TestSumWithIntegers(t *testing.T) {
 
 	tests := []testsTable[int]{
 		{description: "Should sum two numbers of type integer", numbers: []int{1, 2}, want: 3},
@@ -21,6 +21,22 @@ func TestSum(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
 			got := Sum[int](test.numbers[:]...)
+
+			assert.Equal(t, test.want, got, test.description)
+		})
+	}
+}
+
+func TestSumWithFloats(t *testing.T) {
+
+	tests := []testsTable[float32]{
+		{description: "Should sum two numbers of type float32", numbers: []float32{1.3, 2.4}, want: 3.7},
+		{description: "Should sum four numbers of type float32", numbers: []float32{1.1, 2.2, 3.3, 5.5}, want: 12.10},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got := Sum[float32](test.numbers[:]...)
 
 			assert.Equal(t, test.want, got, test.description)
 		})
