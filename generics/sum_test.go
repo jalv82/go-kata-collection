f256a889569f2c418d9de1411df73e70bd0f957c
jalv <jalv82@gmail.com>
2022-05-14 21:18:29 +0200

    Added table test

--- generics/sum_test.go
+++ generics/sum_test.go
@@ -5,10 +5,21 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	got := Sum(2, 3, 5)
+func Test(t *testing.T) {
+	tests := []struct {
+		description string
+		numbers     []int
+		want        int
+	}{
+		{description: "Should sum two numbers of type integer", numbers: []int{1, 2}, want: 3},
+		{description: "Should sum four numbers of type integer", numbers: []int{1, 2, 3, 5}, want: 11},
+	}
 
-	want := 10
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got := Sum(test.numbers[:]...)
 
-	assert.Equal(t, want, got, "Should sum any quantity of numbers of type integer")
+			assert.Equal(t, test.want, got, test.description)
+		})
+	}
 }
