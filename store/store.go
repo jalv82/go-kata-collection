941cad87ef8d0c8dcc63461594bc4949c399a0c9
jalv <jalv82@gmail.com>
2022-05-14 16:36:00 +0200

    When stock is not available is notified

--- store/store.go
+++ store/store.go
@@ -1,11 +1,19 @@
 package store
 
+import "errors"
+
 type Record struct {
 	Title  string
 	Artist string
 	Copies uint
 }
 
-func (r *Record) Buy(quantity uint) {
+func (r *Record) Buy(quantity uint) error {
+	if r.Copies == 0 {
+		return errors.New("error: not exists any copies to sell")
+	}
+
 	r.Copies -= quantity
+
+	return nil
 }
--- store/store_test.go
+++ store/store_test.go
@@ -1,6 +1,9 @@
 package store
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestBuy(t *testing.T) {
 	tests := []struct {
@@ -10,22 +13,29 @@ func TestBuy(t *testing.T) {
 		want        uint
 	}{
 		{
-			description: "Should be nine copies left  when exists ten copies and ones is bought",
+			description: "Should be nine copies left when exists ten copies and ones is bought",
 			record:      Record{Title: "The Jar Dance", Artist: "Chiquito de la calzada", Copies: 10},
 			copiesToBuy: 1,
 			want:        9,
+		},
+		{
+			description: "Should return an error when exists zero copies and ones is bought",
+			record:      Record{Title: "The Jar Dance", Artist: "Chiquito de la calzada", Copies: 0},
+			copiesToBuy: 1,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			test.record.Buy(test.copiesToBuy)
+
+			err := test.record.Buy(test.copiesToBuy)
+			if err != nil {
+				assert.Error(t, err)
+			}
 
 			got := test.record.Copies
 
-			if got != test.want {
-				t.Errorf("Got %v != Want %v", got, test.want)
-			}
+			assert.Equal(t, test.want, got)
 		})
 	}
 }
