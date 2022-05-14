581b21a83db2d16c5551cd0dfe7100ec774e835d
jalv <jalv82@gmail.com>
2022-05-14 14:30:23 +0200

    Refactor to use table tests

--- store/store_test.go
+++ store/store_test.go
@@ -3,19 +3,29 @@ package store
 import "testing"
 
 func TestBuy(t *testing.T) {
-
-	record := Record{
-		Title:  "The Jar Dance",
-		Artist: "Chiquito de la calzada",
-		Copies: 10,
+	tests := []struct {
+		description string
+		record      Record
+		copiesToBuy uint
+		want        uint
+	}{
+		{
+			description: "Should be nine copies left  when exists ten copies and ones is bought",
+			record:      Record{Title: "The Jar Dance", Artist: "Chiquito de la calzada", Copies: 10},
+			copiesToBuy: 1,
+			want:        9,
+		},
 	}
 
-	record.Buy(1)
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			test.record.Buy(test.copiesToBuy)
 
-	got := record.Copies
-	var want uint = 9
+			got := test.record.Copies
 
-	if got != want {
-		t.Errorf("Got %v != Want %v", got, want)
+			if got != test.want {
+				t.Errorf("Got %v != Want %v", got, test.want)
+			}
+		})
 	}
 }
