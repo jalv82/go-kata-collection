61cdff9d5bdcc1ef2bdcfba9ef623af2412bd4e8
jalv <jalv82@gmail.com>
2022-05-14 21:43:01 +0200

    Added generics

--- generics/sum.go
+++ generics/sum.go
@@ -1,7 +1,11 @@
 package generics
 
-func Sum(numbers ...int) int {
-	var result int
+type MyNumber interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Sum[T MyNumber](numbers ...T) T {
+	var result T
 
 	for _, number := range numbers {
 		result += number
--- generics/sum_test.go
+++ generics/sum_test.go
@@ -5,19 +5,22 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
-	tests := []struct {
-		description string
-		numbers     []int
-		want        int
-	}{
+type testsTable[T MyNumber] struct {
+	description string
+	numbers     []T
+	want        T
+}
+
+func TestSum(t *testing.T) {
+
+	tests := []testsTable[int]{
 		{description: "Should sum two numbers of type integer", numbers: []int{1, 2}, want: 3},
 		{description: "Should sum four numbers of type integer", numbers: []int{1, 2, 3, 5}, want: 11},
 	}
 
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
-			got := Sum(test.numbers[:]...)
+			got := Sum[int](test.numbers[:]...)
 
 			assert.Equal(t, test.want, got, test.description)
 		})
