a2be0913513139e1b867f9ea90709320a453e730
jalv <jalv82@gmail.com>
2022-05-27 09:49:54 +0200

    Kata Bitcoin wallet - Added table tests

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -14,3 +14,23 @@ func TestDeposit(t *testing.T) {
 
 	assert.Equal(t, want, got)
 }
+
+func Test(t *testing.T) {
+	tests := []struct {
+		description string
+		wallet      Wallet
+		amount      BTC
+	}{
+		{description: "Should deposit 22.0 BTC on the wallet", wallet: Wallet{}, amount: 22.0},
+		{description: "Should deposit 1.11 BTC on the wallet", wallet: Wallet{}, amount: 1.11},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			test.wallet.Deposit(test.amount)
+			got := test.wallet.Balance()
+
+			assert.Equal(t, test.amount, got, test.description)
+		})
+	}
+}
